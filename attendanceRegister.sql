018a648ff9ac35b71b731a0fb5e51eb6bfd5e1ab
Uyiosa <hiskonxeptz@gmail.com>
2016-10-24 17:25:25 +0100

    Added a report dbase with a Foreign Key

--- attendanceRegister.sql
+++ attendanceRegister.sql
@@ -6,6 +6,12 @@ CREATE TABLE `user_data` (
 	`username`	TEXT NOT NULL UNIQUE,
 	`password`	TEXT NOT NULL DEFAULT '21232f297a57a5a743894a0e4a801fc3'
 );
+CREATE TABLE `report` (
+	`user_id`	INTEGER NOT NULL UNIQUE,
+	`onsite_day1`	INTEGER DEFAULT 0,
+	`onsite_day2`	INTEGER DEFAULT 0,
+	FOREIGN KEY(`user_id`) REFERENCES user_data(id)
+);
 CREATE TABLE `event_list` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`events`	TEXT NOT NULL,
